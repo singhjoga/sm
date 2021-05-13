6f2c1560f367f6f831aaa369d5a4b3b3274a722a
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-05-13 12:11:27 +0200

    REfactoring

---
+++ api/src/test/java/com/spydermama/api/SmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spydermama.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
