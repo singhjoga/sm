be4a00b8c29494848c6a4c38d087ad3ad809364e
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-08-27 17:28:37 +0200

    Refactoring

--- api/src/main/java/com/spydermama/api/SmApiApplication.java
+++ api/src/main/java/com/spydermama/api/SmApiApplication.java
@@ -2,10 +2,18 @@ package com.spydermama.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@ComponentScan(basePackages = {"com.spydermama.api","com.technovator.api"})
+@EnableJpaRepositories(basePackages = {"com.spydermama.api","com.technovator.api"})
+@EntityScan(basePackages= {"com.spydermama.api","com.technovator.api"})
+@Configuration
 public class SmApiApplication {
 
 	public static void main(String[] args) {
